6b52d1bffd078c524e2dc7fc17e111e291f5a48a
luciapuccini <puccinilucia@gmail.com>
2018-08-15 23:56:00 -0300

    Basic tests to try jest

--- functions.js
+++ functions.js
@@ -1,5 +1,14 @@
 const functions = {
-    add: (num1, num2) => num1 + num2
-}
+    add: (num1, num2) => num1 + num2,
+    isNull: () => null,
+    checkValue: (x) => x,
+    createUser: () => {
+        const user = {
+            firstName: 'Lucia'
+        };
+        user['lastName'] = 'Puccini';
+        return user
+    }
+};
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,45 @@
-const functions = require('./functions');
+const functions = require("./functions");
 
-test('Adds 2+2 = 4', () => {
+test("Adds 2+2 = 4", () => {
     expect(functions.add(2, 2)).toBe(4);
+});
+
+test("Adds 2+2 to not equal 5", () => {
+    expect(functions.add(2, 2)).not.toBe(5);
+});
+//toBeNull
+test('Should be null', () => {
+    expect(functions.isNull()).toBeNull();
+});
+//toBeFalsy
+test('Should be falsy', () => {
+    expect(functions.checkValue(null)).toBeFalsy();
+});
+//toBeTruthy
+test('Should be truthy', () => {
+    expect(functions.checkValue(true)).toBeTruthy();
+});
+//toEqual
+test('User should be Lucia Puccini', () => {
+    expect(functions.createUser()).toEqual({
+        firstName: 'Lucia',
+        lastName: 'Puccini'
+    });
+});
+
+// Less than and greater than
+test('should be under 1600', () => {
+    const load1 = 800;
+    const loan2 = 700;
+    expect(load1 + loan2).toBeLessThan(1600);
+});
+
+//Regex
+test('there is no I in team', () => {
+    expect('team').not.toMatch(/I/i);
+});
+//arrays
+test('Admin should be in usernames', () => {
+    usernames = ['jose', 'karen', 'admin'];
+    expect(usernames).toContain('admin');
 });
